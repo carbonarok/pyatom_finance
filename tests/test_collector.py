41f5f6e23057b4ad0af1400da7d5dbd715855d13
carbonarok <carbonarok@gmail.com>
2021-03-11 16:59:02 +0000

    Improved test coverage

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -8,47 +8,56 @@ class TestCollector(TestCase):
     def setUp(self):
         config.load()
         self.collector = Collector()
+        self.symbol = "CSCO"
 
     def test_get_better_consensuses(self):
-        response = self.collector.get_better_consensuses("CSCO")
+        response = self.collector.get_better_consensuses(self.symbol)
         self.assertNotEqual(response["data"]["symbol"]["betterConsensuses"], None)
 
     def test_get_brokerage_actions(self):
-        response = self.collector.get_brokerage_actions("CSCO")
+        response = self.collector.get_brokerage_actions(self.symbol)
         self.assertNotEqual(response["data"]["symbols"][0]["brokerageActions"], None)
 
     def test_get_chart_interday(self):
-        response = self.collector.get_chart_interday("CSCO")
+        response = self.collector.get_chart_interday(self.symbol)
         self.assertNotEqual(response["data"]["interday"], None)
 
     def test_get_chart_intraday(self):
-        response = self.collector.get_chart_intraday("CSCO")
+        response = self.collector.get_chart_intraday(self.symbol)
         self.assertNotEqual(response["data"]["intraday"], None)
 
     def test_get_financial_data(self):
-        response = self.collector.get_financial_data("CSCO")
+        response = self.collector.get_financial_data(self.symbol)
         self.assertNotEqual(response["data"]["symbol"]["ratiosData"], None)
 
+    def test_get_future_events(self):
+        response = self.collector.get_future_events(self.symbol)
+        self.assertNotEqual(response["data"]["manyEvents"]["futureEvents"], None)
+
     def test_get_market_cap(self):
-        response = self.collector.get_market_cap("CSCO")
+        response = self.collector.get_market_cap(self.symbol)
         self.assertNotEqual(response["data"]["symbol"]["metric"]["atomMarketCap"], None)
 
     def test_get_news_feed(self):
-        response = self.collector.get_news_feed("CSCO")
+        response = self.collector.get_news_feed(self.symbol)
         self.assertNotEqual(response["data"]["newsFeed"], None)
 
     def test_get_overview_data(self):
-        response = self.collector.get_overview_data("CSCO")
+        response = self.collector.get_overview_data(self.symbol)
         self.assertNotEqual(response["data"]["symbol"], None)
 
+    def test_get_past_events(self):
+        response = self.collector.get_past_events(self.symbol)
+        self.assertNotEqual(response["data"]["manyEvents"]["pastEvents"], None)
+
     def test_get_period_guidance(self):
-        response = self.collector.get_period_guidance("CSCO")
+        response = self.collector.get_period_guidance(self.symbol)
         self.assertNotEqual(response["data"]["symbol"]["quarterlyGuidance"], None)
 
     def test_get_relative_consensuses(self):
-        response = self.collector.get_relative_consensuses("CSCO")
+        response = self.collector.get_relative_consensuses(self.symbol)
         self.assertNotEqual(response["data"]["symbol"]["relativeConsensuses"], None)
 
     def test_get_symbol(self):
-        response = self.collector.get_symbol("CSCO")
+        response = self.collector.get_symbol(self.symbol)
         self.assertNotEqual(response["data"]["symbol"]["symbol"], None)
